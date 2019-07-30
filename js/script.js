1fdafdbf8933405a071924036b77f7b15d6a290c
Sasha Kolachko <kolachko999@gmail.com>
2019-07-30 16:08:16 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+let perimeter = (a, b) => {
+  if (b == undefined) {
+    return squarePerimeter(a);
+  } else {
+    return rectanglePerimeter(a, b);
+  }
+}
+
+let rectanglePerimeter = (a, b) => 2 * (a + b);
+let squarePerimeter = (a) => 4 * a;
+
+console.log(perimeter(2, 4));
+console.log(perimeter(8));
+
+let perimeterNew = (a, b) => {
+
+  return new Promise(function(resolve, reject) {
+    if (a == undefined && b == undefined) {
+      reject("No parameters");
+    }
+    if (b == undefined) {
+      return resolve(squarePerimeter(a));
+    }
+    return resolve(rectanglePerimeter(a, b));
+  });
+}
+
+perimeterNew(13, 45)
+  .then(result => console.log(result))
+  .catch(error => console.log(error));
+
+perimeterNew(13)
+  .then(result => console.log(result))
+  .catch(error => console.log(error));
+perimeterNew()
+  .then(result => console.log(result))
+  .catch(error => console.log(error));
